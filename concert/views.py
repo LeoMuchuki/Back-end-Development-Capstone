6ffd6714d53d97318920961fdf5c8321f0eeea18
Leonard Muchuki <lennymkuu@gmail.com>
2024-06-02 18:25:35 -0400

    finished the main django project

--- concert/views.py
+++ concert/views.py
@@ -22,16 +22,13 @@ def signup(request):
             if user:
                 return render(request, "signup.html", {"form": SignUpForm, "message": "user already exist"})
             else:
-                user = user.objects.create(
-                    username = username, password = make_password(password))
-                    login(request, user)
-                    return HttpResponseRedirect(reverse("index"))
-        
-        except.User.DoesNotExist:
+                user = User.objects.create(
+                    username=username, password=make_password(password))
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+        except User.DoesNotExist:
             return render(request, "signup.html", {"form": SignUpForm})
-                
     return render(request, "signup.html", {"form": SignUpForm})
-
 
 def index(request):
     return render(request, "index.html")
@@ -61,13 +58,43 @@ def photos(request):
     return render(request, "photos.html", {"photos": photos})
 
 def login_view(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        try:
+            user = User.objects.get(username=username)
+
+            if user.check_password(password):
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+        except User.DoesNotExist:
+            return render(request, "login.html", {"form": LoginForm})
+    return render(request, "login.html", {"form": LoginForm})
+
 
 def logout_view(request):
-    pass
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
 
 def concerts(request):
-    pass
+    if request.user.is_authenticated:
+        concerts_list = list()
+        concerts_objects = Concert.objects.all()
+        for item in concerts_objects:
+            try:
+                status = item.attendee.filter(
+                    user=request.user).first().attending
+            
+            except:
+                status = "-"
+                concerts_list.append({
+                    "concert": item,
+                    "status": status
+                })
+
+        return render(request, "concerts.html", {"concerts": concerts_list})
+    else:
+        return HttpResponseRedirect(reverse("login"))
 
 
 def concert_detail(request, id):
